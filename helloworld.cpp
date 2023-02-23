9bc89a9102c2ded4020d7a709e57b1d24179e432
georgegoldman <georgegoldmanjohn.o@gmail.com>
2023-02-23 15:57:05 +0100

    fixed

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-  int j = 5;
+  // int j = 5;
   // printf("%d\n", k);
   // int sheep_counter = 0;
   // sheep_counter = sheep_counter + 1;
@@ -23,16 +23,62 @@ int main()
   // int i = static_cast<int>(f);
   // cout << i  << endl;
  
-  int value;
-  cout << "Give me a number and i will square it!\n";
-  cin>> value;
+  // int value;
+  // cout << "Give me a number and i will square it!\n";
+  // cin>> value;
 
   // int square = value * value;
   // cout << "You'v given "<< value << endl;
   // cout << "the squared value is "<< square << endl;
-  if(value >= 0.0) {
-    float squareroot = sqrtf(value);
-    cout<< "You'v given: "<< value << endl;
-    cout << "the sqaure root is: "<< squareroot << endl;
-  }
-}
+  // if(value >= 0.0) {
+  //   float squareroot = sqrtf(value);
+    // cout<< "You'v given: "<< value << endl;
+    // cout << "the sqaure root is: "<< squareroot << endl;
+  // }
+  // int i = 10, j = 3;
+  // int k = (i % j * i /3  )/(j % i- j /i);
+  // cout << k << endl;
+  // char z = 'Z';
+  // cout<< (int)z << endl;
+
+  // float a;
+  // cin >> a;
+  // a *= a;
+  // a *= a;
+  // a = sqrtf (a) * sqrtf(a);
+  // cout << a ;
+  // cout << "hi"<< endl;
+  // char character = '\x27';
+  // cout << character << endl;
+  // char character = 'X';
+  // int integer  = character;
+  // cout << character << " " << static_cast<int>(character) << endl;
+  // cout << character << " " << static_cast<char>(character) << endl;
+  // int i = 10, j = 3;
+  // int k = (i % j * i / 3) / (j % i - j / i);
+  // cout << k << endl;
+  // char character = 'Z';
+  // cout << static_cast<int>(character) << endl;
+  // int i, j;
+  // cin >> i;
+  // j = i;
+  // if (i >= 9)
+  //   i += 9 - i;
+  // j /= i;
+  // cout << j;
+
+  // float a, b;
+  // cin >> a;
+  // if (b >= 1)
+  //   a += b;
+  // if (a > b)
+  //   b += a;
+  // cout << a + b << endl;
+
+//   float a;
+//   cin >> a;
+//   a *= a;
+//   a *= a;
+//   a = sqrtf (a) * sqrtf(a);
+//   cout << a;
+//  }
